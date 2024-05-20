e9a3dba8f7f5d6c0c11ebb12f53433f694a5335a
Pepega1412 <68907123+Pepega1412@users.noreply.github.com>
2024-05-21 00:54:43 +0700

    Add files via upload

--- src/main/java/com/example/store/dao/OrderDAO.java
+++ src/main/java/com/example/store/dao/OrderDAO.java
@@ -7,20 +7,18 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import com.example.store.model.Contact;
-import com.example.store.model.Lead;
 import com.example.store.model.Order;
 import com.example.store.model.OrderItem;
-import com.example.store.model.User;
 
 public class OrderDAO {
 	static final String GET_ORDERS = "SELECT * FROM orders, contacts WHERE orders.contact = contacts.id";
 	static final String GET_ORDER_BY_ID = "SELECT * FROM orders, contacts WHERE orders.contact = contacts.id AND orders.id=?";
-	static final String SAVE_LEAD = "INSERT INTO `qa_store`.`leads` (`lead_owner`, `name`, `company`, `title`, `email`, `phone`, `website`, `lead_source`, `lead_status`, `rating`) "
-			+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
-	static final String UPDATE_LEAD = "UPDATE `qa_store`.`leads` "
-			+ "SET `lead_owner` = ?, `name` = ?, `company` = ?, `title` = ?, `email` = ?, `phone` = ?, `website` = ?, `lead_source` = ?, `lead_status` = ?, `rating` = ? "
+	static final String SAVE_ORDER = "INSERT INTO `qa_store`.`orders` (`contact`, `status`, `address`, `due_date`) "
+			+ "VALUES (?, ?, ?, ?);";
+	static final String UPDATE_ORDER = "UPDATE `qa_store`.`orders` "
+			+ "SET `contact` = ?, `status` = ?, `address` = ?, `due_date` = ? "
 			+ "WHERE (`id` = ?);";
-	static final String DELETE_LEAD = "DELETE FROM `qa_store`.`leads` WHERE (`id` = ?);";
+	static final String DELETE_ORDER = "DELETE FROM `qa_store`.`orders` WHERE (`id` = ?);";
 	
 	public static ArrayList<Order> getOrders() {
 		ArrayList<Order> orders = new ArrayList<Order>();
@@ -54,9 +52,9 @@ public class OrderDAO {
 			if(rs.next()) {
 				Order order = new Order();
 				order.setId(rs.getInt(1));
-				order.setDue_date(rs.getDate(3));
-				order.setStatus(rs.getString(4));
-				order.setAddress(rs.getString(5));
+				order.setDue_date(rs.getDate(2));
+				order.setStatus(rs.getString(3));
+				order.setAddress(rs.getString(4));
 				Contact contact = new Contact();
 				contact.setId(rs.getInt(6));
 				contact.setName(rs.getString(8));
@@ -80,36 +78,22 @@ public class OrderDAO {
 			if(rs.next()) {
 				OrderItem orderItem = new OrderItem();
 				orderItem.setAmount(rs.getInt(3));
-				order.setDue_date(rs.getDate(3));
-				order.setStatus(rs.getString(4));
-				order.setAddress(rs.getString(5));
-				Contact contact = new Contact();
-				contact.setId(rs.getInt(6));
-				contact.setName(rs.getString(8));
-				order.setContact(contact);
-				
-				return order;
+				orderItems.add(orderItem);
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 		}
-		return null;
+		return orderItems;
 	}
 	
 	public static void saveOrder(Order order) {
 		try (Connection connection = DAO.getConnection(); 
-				PreparedStatement ps = connection.prepareStatement(SAVE_LEAD)) {
-			ps.setInt(1, lead.getLead_owner().getId());
-			ps.setString(2, lead.getName());
-			ps.setString(3, lead.getCompany());
-			ps.setString(4, lead.getTitle());
-			ps.setString(5, lead.getEmail());
-			ps.setString(6, lead.getPhone());
-			ps.setString(7, lead.getWebsite());
-			ps.setString(8, lead.getLead_source());
-			ps.setString(9, lead.getLead_status());
-			ps.setString(10, lead.getRating());
+				PreparedStatement ps = connection.prepareStatement(SAVE_ORDER)) {
+			ps.setInt(1, order.getContact().getId());
+			ps.setString(3, order.getStatus());
+			ps.setString(4, order.getAddress());
+			ps.setDate(5, order.getDue_date());
 			ps.execute();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -117,20 +101,14 @@ public class OrderDAO {
 		}
 	}
 	
-	public static void updateLead(Lead lead) {
+	public static void updateLead(Order order) {
 		try (Connection connection = DAO.getConnection(); 
-				PreparedStatement ps = connection.prepareStatement(UPDATE_LEAD)) {
-			ps.setInt(1, lead.getLead_owner().getId());
-			ps.setString(2, lead.getName());
-			ps.setString(3, lead.getCompany());
-			ps.setString(4, lead.getTitle());
-			ps.setString(5, lead.getEmail());
-			ps.setString(6, lead.getPhone());
-			ps.setString(7, lead.getWebsite());
-			ps.setString(8, lead.getLead_source());
-			ps.setString(9, lead.getLead_status());
-			ps.setString(10, lead.getRating());
-			ps.setInt(11, lead.getId());
+				PreparedStatement ps = connection.prepareStatement(UPDATE_ORDER)) {
+			ps.setInt(1, order.getContact().getId());
+			ps.setString(2, order.getStatus());
+			ps.setString(3, order.getAddress());
+			ps.setDate(4, order.getDue_date());
+			ps.setInt(11, order.getId());
 			ps.executeUpdate();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -138,9 +116,9 @@ public class OrderDAO {
 		}
 	}
 	
-	public static void deleteLead(int id) {
+	public static void deleteOrder(int id) {
 		try (Connection connection = DAO.getConnection(); 
-				PreparedStatement ps = connection.prepareStatement(DELETE_LEAD)) {
+				PreparedStatement ps = connection.prepareStatement(DELETE_ORDER)) {
 			ps.setInt(1, id);
 			ps.executeUpdate();
 		} catch (SQLException e) {
